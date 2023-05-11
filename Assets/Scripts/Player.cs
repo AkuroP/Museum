4ca7565ff7854255bb5174f392be79b33ea214ee
AkuroP <thomas.kang2002@gmail.com>
2023-05-11 12:49:27 +0200

    Object activate/deactivate
    + Object activate/deactivate when on/off camera

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("Vision du joueur")]
+    [Range(0f, 360f)]
+    public float fov = 180f;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        /*
+            Indicate FOV of Camera
+        */
+        //Cercle
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(this.transform.position, 1);
+        
+        //Rayon droit
+        Gizmos.color = Color.red;
+        Quaternion quaternion = Quaternion.AngleAxis(fov / 2, Vector3.up);
+        Vector3 yes = quaternion * this.transform.forward;
+        Gizmos.DrawRay(this.transform.position, yes * 10);
+        
+        //Rayon gauche
+        Quaternion quaternion2 = Quaternion.AngleAxis(fov / 2, Vector3.down);
+        Vector3 yes2 = quaternion2 * this.transform.forward;
+        Gizmos.DrawRay(Camera.main.transform.position, yes2 * 10);
+    }
+}
---
+++ Assets/Scripts/SchrodingerObject.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SchrodingerObject : MonoBehaviour
+{
+    private Vector3 dir;
+    private Renderer ObjectRenderer;
+    private Collider col;
+    private Player player;
+    public bool deactiveCollider = true;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        ObjectRenderer = this.GetComponent<Renderer>();
+        col = this.GetComponent<Collider>();
+        player = Camera.main.GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //calculate direction of object
+        dir = this.transform.position - Camera.main.transform.position;
+
+        /*
+            Active Object renderer in Camera view
+        */
+        if(!ObjectRenderer.enabled)return;
+        if(ObjectRenderer.isVisible)
+        {
+            ObjectRenderer.enabled = true;
+            if(deactiveCollider && col != null)col.enabled = true;
+        }
+
+        /*
+            Deactive object renderer when not in camera view 
+        */
+        if(Vector3.Angle(dir, Camera.main.transform.forward) > player.fov)
+        {
+            if(ObjectRenderer.isVisible)return;
+            ObjectRenderer.enabled = false;
+            if(deactiveCollider && col != null)col.enabled = false;
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        Debug.Log($"{this.name} became invisible");
+        //ObjectRenderer.enabled = false;
+    }
+
+    private void OnBecameVisible()
+    {
+        Debug.Log($"{this.name} became visible !");
+        //ObjectRenderer.enabled = true;
+    }
+}
