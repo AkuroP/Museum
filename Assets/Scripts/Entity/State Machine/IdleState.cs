0240c2ba858d1756426af7b43db2fb11fd15450d
AkuroP <thomas.kang2002@gmail.com>
2023-05-31 16:28:44 +0200

    Modified tunnelling + integration
    + integration dragon + lezard
    ~player prefab

--- Assets/Scripts/Entity/State Machine/IdleState.cs
+++ Assets/Scripts/Entity/State Machine/IdleState.cs
@@ -5,22 +5,55 @@ using UnityEngine;
 public class IdleState : EntityState
 {
     private MeshRenderer entityRenderer;
+    [SerializeField]
+    private bool isAnimal;
+    [Range(0, 10)]
+    [SerializeField]
+    private float maxTimeChangeState;
+
+    private float timeChangeState;
+
+    [SerializeField]
+    private float timer = 0f;
     public override void EnterState(Entity state)
     {
         //Debug.Log("Enter Idle State");
+        state.Agent.isStopped = true;
         entityRenderer = state.GetComponentInChildren<MeshRenderer>();
+        timeChangeState = Random.Range(0, maxTimeChangeState);
     }
 
     public override void UpdateState(Entity state)
     {
-        if(Vector3.Distance(this.transform.position, state.Player.transform.position) > (state.Player.NavMeshAgent.radius + state.Agent.stoppingDistance))
-        {
-            entityStateMachine.ChangeState(state.FollowState);
-        }
+        CheckPlayerDistance(state);
+        WaitBeforeChangingState(state);
         /*
             - regarde le joueur s'il est visible
         if(entityRenderer.isVisible)this.transform.LookAt(state.Player.transform.position);
         */
+    }
+
+    private void CheckPlayerDistance(Entity state)
+    {
+        if (!state.FollowState.FollowPlayer) return;
+        if (Vector3.Distance(this.transform.position, state.Player.transform.position) > (state.Player.NavMeshAgent.radius + state.Agent.stoppingDistance))
+        {
+            entityStateMachine.ChangeState(state.FollowState);
+        }
+    }
+
+    private void WaitBeforeChangingState(Entity state)
+    {
+        if (!isAnimal) return;
+        if(timer >= timeChangeState)
+        {
+            entityStateMachine.ChangeState(state.WanderState);
+            timer = 0f;
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
     }
 
     public override void ExitState(Entity state)
