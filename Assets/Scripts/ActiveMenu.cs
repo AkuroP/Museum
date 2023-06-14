b1f76ecd925057bca5f558d92b39872b525946be
AntoineEsbelin <antoine.esbelin@edu.devinci.fr>
2023-06-14 16:24:50 +0200

    PlayerPrefab modif

--- Assets/Scripts/ActiveMenu.cs
+++ Assets/Scripts/ActiveMenu.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ActiveMenu : MonoBehaviour
 {
     public GameObject panel;
+    public GameObject fxMontre;
     void Start()
     {
         
@@ -12,11 +13,16 @@ public class ActiveMenu : MonoBehaviour
 
     void Update()
     {
-        if (gameObject.transform.eulerAngles.z <= 276f && gameObject.transform.eulerAngles.z >= 252f)
+        if (gameObject.transform.eulerAngles.z <= 276f && gameObject.transform.eulerAngles.z >= 230f)
         {
             panel.SetActive(true);
+            fxMontre.SetActive(true);
+
         }
         else
+        {
             panel.SetActive(false);
+            fxMontre.SetActive(false);
+        }
     }
 }
