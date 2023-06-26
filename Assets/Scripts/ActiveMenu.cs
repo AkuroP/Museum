d7cc672496d25bf15f8213a2710b7ee6a659be14
AkuroP <thomas.kang2002@gmail.com>
2023-06-26 10:41:51 +0200

    montre

--- Assets/Scripts/ActiveMenu.cs
+++ Assets/Scripts/ActiveMenu.cs
@@ -8,7 +8,11 @@ public class ActiveMenu : MonoBehaviour
     public GameObject fxMontre;
 
     private bool panelShowed;
-    private bool panelHided;
+    private bool panelHided = true;
+    [SerializeField]
+    private float MontreValue;
+
+    private float value;
     void Start()
     {
         
@@ -16,24 +20,33 @@ public class ActiveMenu : MonoBehaviour
 
     void Update()
     {
-        if (gameObject.transform.eulerAngles.z <= 276f && gameObject.transform.eulerAngles.z >= 230f)
+        if (gameObject.transform.eulerAngles.z <= 276f + value && gameObject.transform.eulerAngles.z >= 230f - value)
         {
             panel.SetActive(true);
-            
-            /*if (!panelShowed) AudioManager.instance.PlayClipAt(AudioManager.instance.allAudio["SFX_OpenHUD"], this.transform.position, AudioManager.instance.soundEffectMixer, true, false);
-            panelShowed = true;
-            panelHided = false;*/
+
+            if (!panelShowed)
+            {
+                AudioManager.instance.PlayClipAt(AudioManager.instance.allAudio["SFX_OpenHUD"], this.transform.position, AudioManager.instance.soundEffectMixer, true, false);
+                panelShowed = true;
+                panelHided = false;
+                value = MontreValue;
+            }
         }
         else
         {
-            /*if (!panelHided) AudioManager.instance.PlayClipAt(AudioManager.instance.allAudio["SFX_CloseHUD"], this.transform.position, AudioManager.instance.soundEffectMixer, true, false);
-            panelHided = true;
-            panelShowed = false;*/
+            if (!panelHided)
+            {
+                AudioManager.instance.PlayClipAt(AudioManager.instance.allAudio["SFX_CloseHUD"], this.transform.position, AudioManager.instance.soundEffectMixer, true, false);
+                panelHided = true;
+                panelShowed = false;
+                value = 0f;
+            }
+
             panel.SetActive(false);
 
         }
 
-        if (gameObject.transform.eulerAngles.z <= 300f && gameObject.transform.eulerAngles.z >= 205f)
+        if (gameObject.transform.eulerAngles.z <= 300f + value && gameObject.transform.eulerAngles.z >= 205f - value)
         {
             fxMontre.SetActive(true);
         }
